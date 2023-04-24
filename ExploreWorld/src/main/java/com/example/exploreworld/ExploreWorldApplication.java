041911620db86799a40a4f3a97c23378ccd100aa
Saeid Amini <amini.com@gmail.com>
2023-04-23 22:07:42 -1100

    Init Explore the world microservice.

---
+++ ExploreWorld/src/main/java/com/example/exploreworld/ExploreWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.exploreworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreWorldApplication.class, args);
+	}
+
+}
---
+++ ExploreWorld/src/test/java/com/example/exploreworld/ExploreWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exploreworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
